16aeaba3feb3a4a3a840b0aa10fa38d3fc42beeb
Fhilipe Coelho <coelhocrash11@gmail.com>
2021-06-15 20:09:06 -0300

    config helper script

---
+++ .bin/config.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+if [ $1 == "" ]
+then
+  echo "Digite nvim para abrir a configuração"
+elif [ $1 == "nvim" ]
+then
+  $EDITOR ~/.config/nvim/init.vim
+elif [ $1 == "polybar" ]
+then 
+  $EDITOR ~/.config/polybar/config.ini ~/.config/polybar/bars.ini ~/.config/polybar/modules.ini
+elif [ $1 == "bspwm" ]
+then 
+  $EDITOR ~/.config/bspwm/bspwmrc
+elif [ $1 == "sxhkd" ]
+then
+  $EDITOR ~/.config/sxhkd/sxhkdrc
+elif [ $1 == "zsh" ]
+then 
+  $EDITOR ~/.zshrc
+elif [ $1 == "dunst" ]
+then
+  $EDITOR ~/.config/dunst/dunstrc
+elif [ $1 == "alacritty" ]
+then
+  $EDITOR ~/.config/dunst/dunstrc
+elif [ $1 == "picom" ]
+then
+  $EDITOR ~/.config/picom/picom.conf
+fi
+

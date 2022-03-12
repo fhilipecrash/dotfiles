e2d402e613e7787cb46d27399141754581de3730
Fhilipe Coelho <coelhocrash11@gmail.com>
2022-03-12 17:16:34 -0300

    add pwsh configs

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,11 @@
 # Alias
 Set-Alias lvim $HOME\.local\bin\lvim.ps1
 
+# Import Modules
+Import-Module Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+
 oh-my-posh --init --shell pwsh --config $env:POSH_THEMES_PATH\powerlevel10k_rainbow.omp.json | Invoke-Expression

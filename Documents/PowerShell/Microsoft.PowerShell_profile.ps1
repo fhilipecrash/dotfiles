cb0e282b5452d038ac464dabb8c0622856e316cd
Fhilipe Coelho <fhilipecoelho.dev@gmail.com>
2023-12-07 11:02:45 -0300

    `feat`: add cd function replace not working yet

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,10 @@
 # Alias
-Set-Alias ll ls
+Set-Alias la ls
+
+function cd {
+    Set-Location -Path @Args
+    $host.ui.RawUI.WindowTitle = "$($Env:UserName)@$(Invoke-Expression -Command 'hostname'):$(pwd)"
+}
 
 function docker {
     wsl -d "Arch" docker @Args
@@ -11,7 +16,7 @@ function docker-compose {
 
 # Import Modules
 Import-Module Terminal-Icons
-Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
+#Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
 
 # PSReadLine
 Set-PSReadLineOption -PredictionSource History
@@ -20,3 +25,4 @@ Set-PSReadLineOption -PredictionViewStyle ListView
 # Start Starship
 Invoke-Expression (&starship init powershell)
 $ENV:STARSHIP_CONFIG = "$HOME\.config\starship\starship.toml"
+#$host.ui.RawUI.WindowTitle = "$($Env:UserName)@$(Invoke-Expression -Command 'hostname'):$(pwd)"

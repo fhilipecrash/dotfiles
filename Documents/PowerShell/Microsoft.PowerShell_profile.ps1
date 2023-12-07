dcbea83fd9b5c7b21073673d7a58347c2922bc11
Fhilipe Coelho <fhilipecoelho.dev@gmail.com>
2023-12-07 11:40:34 -0300

    `feat`: override get-location method

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -6,6 +6,22 @@ function cd {
     $host.ui.RawUI.WindowTitle = "$($Env:UserName)@$(Invoke-Expression -Command 'hostname'):$(pwd)"
 }
 
+function Set-Location {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$false,ValueFromPipeline)]
+        $string
+    )
+    Process {
+        if (!$string) {
+            Microsoft.PowerShell.Management\Set-Location -Path 'C:\Users\fhili'
+            return
+        }
+        Microsoft.PowerShell.Management\Set-Location -Path $string
+        $host.ui.RawUI.WindowTitle = "$($Env:UserName)@$(Invoke-Expression -Command 'hostname'):$(pwd)".replace('C:\Users\fhili', '~')
+    }
+}
+
 function docker {
     wsl -d "Arch" docker @Args
 }
@@ -25,4 +41,4 @@ Set-PSReadLineOption -PredictionViewStyle ListView
 # Start Starship
 Invoke-Expression (&starship init powershell)
 $ENV:STARSHIP_CONFIG = "$HOME\.config\starship\starship.toml"
-#$host.ui.RawUI.WindowTitle = "$($Env:UserName)@$(Invoke-Expression -Command 'hostname'):$(pwd)"
+$host.ui.RawUI.WindowTitle = "$($Env:UserName)@$(Invoke-Expression -Command 'hostname'):$(pwd)".replace('C:\Users\fhili', '~')

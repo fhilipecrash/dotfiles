3f2b5ea11105cbb978f06bbe5fd892a758f55228
Fhilipe Coelho <fhilipecoelho.dev@gmail.com>
2025-01-22 14:00:30 -0300

    feat: use chezmoi to manage dotfiles

--- .config/nvim/lua/base.lua
+++
@@ -1,9 +0,0 @@
-vim.scriptenconding = 'utf-8'
-vim.opt.encoding = 'utf-8'
-vim.opt.fileencoding = 'utf-8'
-
-vim.wo.number = true
-
-vim.opt.title = true
-vim.opt.autoindent = true
-vim.opt.hlsearch = true
--- .config/nvim/lua/maps.lua
+++
@@ -1,6 +0,0 @@
-local keymap = vim.keymap
-vim.g.mapleader = ','
-
-keymap.set('n', '<C-b>', ':NvimTreeToggle<CR>', { silent = true })
-keymap.set('n', '<S-b>', ':NvimTreeFocus<CR>', { silent = true })
-keymap.set('n', '<leader>r', ':so %<CR>')
